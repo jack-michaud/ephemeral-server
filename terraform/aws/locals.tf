730a8125b03c907d5613718c3e3c904f65207986
Jack Michaud <mrmichaud42@gmail.com>
2021-01-17 22:23:28 -0800

    Use correct availability zone for AWS

--- terraform/aws/locals.tf
+++ terraform/aws/locals.tf
@@ -3,5 +3,8 @@ locals {
   user_data = templatefile("${path.module}/user_data.tmpl", {
     public_key_data = file(var.public_key_path)
   })
+  // oh man, limitation. 
+  // TODO Specify availability_zone, or dynamically pick the first one
+  availability_zone = "${var.region}a"
 }
 
