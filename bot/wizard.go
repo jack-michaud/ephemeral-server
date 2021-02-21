32e005d99bdbb688a4f29b8af21fc3af57ee97bc
Jack Michaud <mrmichaud42@gmail.com>
2021-02-21 15:55:53 -0800

    feat(wizard): Add set-role command
    Configures managing role without going through the whole setup wizard.

--- bot/wizard.go
+++ bot/wizard.go
@@ -366,69 +366,20 @@ func InitializeConfigStateMachine(ctx context.Context, kvConn store.IKVStore) St
 
 	askSetupManagingRole := NewState(
 		"ask-setup-managing-role",
-		func(s *discordgo.Session, m *discordgo.MessageCreate, config *Config, args []string) (error, *State) {
-			Guild, err := s.Guild(m.GuildID)
-			possibleRoles := make(discordgo.Roles, 0)
-			if err == nil {
-				possibleRoles = Guild.Roles
-			}
-			fields := make([]*discordgo.MessageEmbedField, 0)
-			for _, role := range possibleRoles {
-				fields = append(fields, &discordgo.MessageEmbedField{
-					Name:  role.Name,
-					Value: role.ID,
-				})
-			}
-			s.ChannelMessageSendComplex(
-				m.ChannelID,
-				&discordgo.MessageSend{
-					Content: "You may optionally set up a role that can tell the server to go up or down. By default, anyone can control this.\n`>skip` to skip this step\n`>role <role ID>` (not the name) to set a role",
-					Embed: &discordgo.MessageEmbed{
-						Fields: fields,
-					},
-				},
-			)
-			if len(fields) == 0 {
-				s.ChannelMessageSend(
-					m.ChannelID,
-					"I was going to ask you if you wanted to set up a role, but for some reason I can't get a list of roles. Sorry... Just `>skip`.",
-				)
-			}
-			return nil, nil
-		},
+		askSetupManagingRoleFunction,
 	)
 	handleSetupManagingRole := NewState(
 		"handle-setup-managing-role",
-		func(s *discordgo.Session, m *discordgo.MessageCreate, config *Config, args []string) (error, *State) {
-			roleId := args[1]
-			Guild, err := s.Guild(m.GuildID)
-			possibleRoles := make(discordgo.Roles, 0)
-			if err != nil {
-				s.ChannelMessageSend(
-					m.ChannelID,
-					"Couldn't get roles...Skipping setting the role ID.",
-				)
-				return nil, nil
-			}
-			possibleRoles = Guild.Roles
-			for _, role := range possibleRoles {
-				if roleId == role.ID {
-					s.ChannelMessageSend(
-						m.ChannelID,
-						fmt.Sprintf("Managing Role: %s\nConfirm? `>yes`/`>no`", role.Name),
-					)
-					config.ManagingRoleId = roleId
-					return nil, nil
-				}
-			}
+		handleSetupManagingRoleFunction,
+	)
 
-			msg := "Invalid role, make sure you're using the numeric role ID."
-			s.ChannelMessageSend(
-				m.ChannelID,
-				msg,
-			)
-			return fmt.Errorf(msg), nil
-		},
+	askSetupManagingRoleFromRoot := NewState(
+		"ask-setup-managing-role",
+		askSetupManagingRoleFunction,
+	)
+	handleSetupManagingRoleFromRoot := NewState(
+		"handle-setup-managing-role",
+		handleSetupManagingRoleFunction,
 	)
 
 	askServerType := NewState(
@@ -496,6 +447,7 @@ func InitializeConfigStateMachine(ctx context.Context, kvConn store.IKVStore) St
 				"`>ephemeral config`: Runs through the setup wizard. For first type setup or full reconfigurations.\n" +
 				"`>ephemeral set-type`: Sets the type of server to boot up.\n" +
 				"`>ephemeral set-size`: Sets the size of server to boot up. Use `>wq` when confirming to skip out of the rest of the wizard.\n" +
+				"`>ephemeral set-role`: Sets the managing role.\n" +
 				"`>ephemeral up`: Brings up the server\n" +
 				"`>ephemeral down`: Takes down the server\n"
 			s.ChannelMessageSend(
@@ -548,7 +500,14 @@ func InitializeConfigStateMachine(ctx context.Context, kvConn store.IKVStore) St
 	rootState.AddState(`^>eph[emeral]* set-type`, askServerType)
 	rootState.AddState(`^>eph[emeral]* set-type (.+)`, handleServerType)
 	rootState.AddState(`^>eph[emeral]* set-size`, askSize)
+	rootState.AddState(`^>eph[emeral]* set-role`, askSetupManagingRoleFromRoot)
 	rootState.AddState(`^>eph[emeral]* help$`, helpStep)
+
+	askSetupManagingRoleFromRoot.AddState(`>cancel`, cancelStep)
+	askSetupManagingRoleFromRoot.AddState(`>role (.+)`, handleSetupManagingRoleFromRoot)
+
+	handleSetupManagingRoleFromRoot.AddState(`>yes`, saveConfig)
+	handleSetupManagingRoleFromRoot.AddState(`>no`, askSetupManagingRoleFromRoot)
 
 	rootState.AddState(`^>eph[emeral]* (up|off|down|wipe|ip)$`, ephemeralCtlState)
 
